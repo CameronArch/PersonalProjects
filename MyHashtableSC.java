f3aab600abd101080632f0a055f14e610a3b4195
CameronArch <cameronarch598@gmail.com>
2023-06-19 18:57:52 -0700

    Finished HashEntry Class

--- MyHashtableSC.java
+++ MyHashtableSC.java
@@ -1,5 +1,23 @@
+/*
+  Name: Cameron Arch
+  Email: cameronarch598@gmail.com
+  Sources Used: Java Interface Documentation
 
+  This file holds the implementation for an entry node in a Hashtable,
+  a Hashtable with separate chaining, and the 
+  methods associated to them.
+*/
 
+/** 
+ * A MyHashtableSC class for implementing a Hashtable with 
+ * separate chaining and associated methods for a Hashtable. 
+ * 
+ * 
+ * Instance variables:
+ * data - Reference to underlying data structure for Hashtable.
+ * size - Reference to amount of elements in Hashtable.
+ * loadFactor - Reference to maximum load factor for Hashtable before resize.
+*/
 class MyHashtableSC<K,V> {
     Object[] data;
     int size;
@@ -8,8 +26,38 @@ class MyHashtableSC<K,V> {
     private static final int DEFAULT_CAPACITY = 11;
     private static final double DEFAULT_LOAD_FACTOR = 0.75;
 
-    private class HashEntry<k,V> {
+    /** 
+    * A HashEntry class for implementing an entry in MyHashtable and
+    * associated methods for an entry. 
+    * 
+    * 
+    * Instance variables:
+    * key - Reference to the key of entry.
+    * value - Reference to the value of entry.
+    * next - Reference to next entry in Linked List for separate chaining.
+    */
+    private class HashEntry<K,V> {
+        private K key;
+        private V value;
+        private HashEntry<K,V> next;
 
+        public HashEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public HashEntry<K,V> getNext() {
+            return next;
+        }
     }
 
     public MyHashtableSC() {
