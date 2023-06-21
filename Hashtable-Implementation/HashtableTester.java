2c25cb15a59ebdb8438e48b6f19fa36cadd66a49
CameronArch <cameronarch598@gmail.com>
2023-06-21 14:55:29 -0700

    added test cases

--- Hashtable-Implementation/HashtableTester.java
+++ Hashtable-Implementation/HashtableTester.java
@@ -6,10 +6,18 @@
   This file holds the test cases for the implementations of 
   MyHashtableSC and MyHashtableLP.
 */
-
+import org.junit.*;
+import static org.junit.Assert.*;
 /** 
 * A HashtableTester class for testing MyHashtableSC and MyHashtableLP. 
 */
-class HashtableTester {
+public class HashtableTester {
 
+    @Test
+    public void contructorSCTest() {
+        MyHashtableSC<String,Integer> test = new MyHashtableSC<>();
+        
+        assertEquals(0,test.size);
+
+    }
 }
