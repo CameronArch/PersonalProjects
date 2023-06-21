dd3a194a23923a6e50c9debbf3b40f0340a91d73
CameronArch <cameronarch598@gmail.com>
2023-06-21 14:34:40 -0700

    created tester file

---
+++ Hashtable-Implementation/HashtableTester.java
@@ -0,0 +1,15 @@
+/*
+  Name: Cameron Arch
+  Email: cameronarch598@gmail.com
+  Sources Used: Java Interface Documentation
+
+  This file holds the test cases for the implementations of 
+  MyHashtableSC and MyHashtableLP.
+*/
+
+/** 
+ * A HashtableTester class for testing MyHashtableSC and MyHashtableLP. 
+*/
+class HashtableTester {
+    
+}
