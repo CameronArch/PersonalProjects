e0af4b4cfe68112ec6de8e4b5c415f80729a1458
CameronArch <cameronarch598@gmail.com>
2023-06-21 15:16:59 -0700

    added contructor test case

--- Hashtable-Implementation/HashtableTester.java
+++ Hashtable-Implementation/HashtableTester.java
@@ -18,6 +18,25 @@ public class HashtableTester {
         MyHashtableSC<String,Integer> test = new MyHashtableSC<>();
         
         assertEquals(0,test.size);
+        assertEquals(11,test.data.length);
+        assertEquals(0.75,test.loadFactor,0.0000001);
 
+        MyHashtableSC<String,Integer> test2 = new MyHashtableSC<>(5);
+
+        assertEquals(0,test2.size);
+        assertEquals(5,test2.data.length);
+        assertEquals(0.75,test2.loadFactor,0.0000001);
+        assertThrows(IllegalArgumentException.class,
+                () -> new MyHashtableSC<>(-1));
+
+        MyHashtableSC<String,Integer> test3 = new MyHashtableSC<>(0,0.5);
+
+        assertEquals(0,test3.size);
+        assertEquals(0,test3.data.length);
+        assertEquals(0.5,test3.loadFactor,0.0000001);
+        assertThrows(IllegalArgumentException.class,
+                () -> new MyHashtableSC<>(1,0));
+        assertThrows(IllegalArgumentException.class,
+                () -> new MyHashtableSC<>(-1,1));
     }
 }
